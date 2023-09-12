7f4d94cea8cde92e9b653c5d38ef063346a7fc26
UsamaWaqas <usamawaqas.m@gmail.com>
2023-09-12 22:44:38 +0500

    updated

---
+++ src/components/intro/intro-content/index.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { BsAwardFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+import girl from "../../../images/girl.png";
+import hand from "../../../images/hand.png";
+import CallToAction from "../../shared/CallToAction";
+import { scrollToSection } from "../../utils/helpers";
+
+import "./style.scss";
+
+const IntroContent = () => {
+    return (
+        <div className="intro-content">
+            <div className="layout">
+                <div className="left-col">
+                    <h1 className="title">
+                        <span className="small-text">
+                            <span className="text">Hello</span>
+                            <span className="icon">
+                                <img src={hand} />
+                            </span>
+                            <span className="text">, I Am</span>
+                        </span>
+                        <span className="big-text">Usama waqas</span>
+                    </h1>
+                    <p className="title">
+                     I am passionate and committed to web development and anything that compliments it. Our Search & Social Media, Video Production & App Development services all strengthen our digital Web Design & Development expertise and allow us to offer a complete package to each client.
+
+                    </p>
+                    <CallToAction
+                        text="Contact me"
+                        action={() => {
+                            scrollToSection("contact");
+                        }}
+                    />
+                </div>
+                <div className="right-col">
+                    <img
+                        src={girl}
+                        alt="Hello I am Keniya Andrew"
+                    />
+
+                    <div className="highlights horizontal">
+                        <div className="icon">
+                            <BsAwardFill />
+                        </div>
+                        <div className="text">Best Design Award</div>
+                    </div>
+
+                    <div className="highlights verticle">
+                        <div className="icon">
+                            <FaUser />
+                        </div>
+                        <div className="text">
+                            <span>4k+</span>
+                            Happy Customers
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default IntroContent;
